239e9c294bb4884cc3f387c65db68fad22b8ec72
selcuk baki <selcukbaki@windowslive.com>
2021-05-04 04:35:13 +0300

    Initial Commit

---
+++ PR36-ParseInsta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PR36-ParseInsta
+//
+//  Created by Selcuk Baki on 4/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
