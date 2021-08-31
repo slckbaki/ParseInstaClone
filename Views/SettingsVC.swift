8a5521c69e27987055de2211d49d71b84588417f
selcuk baki <selcukbaki@windowslive.com>
2021-08-31 20:47:15 +0300

    first commit

---
+++ Views/SettingsVC.swift
@@ -0,0 +1,29 @@
+//
+//  SettingsVC.swift
+//  PR36-ParseInsta
+//
+//  Created by Selcuk Baki on 4/5/21.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
